fddd915c623aa80ead858f123d2e5e0b17c48165
Akash Mondal <mondalakash63@gmail.com>
2024-04-01 04:21:00 +0530

    Again

--- regression.py
+++ regression.py
@@ -1,23 +1,28 @@
 import torch
 from tqdm import tqdm
 
-class Regression:
-    def __init__(self,t_in,t_out):
-        self.t_in = t_in
-        self.t_out = t_out
-        self.w = torch.zeros(t_in.shape[1],requires_grad=True)
-        self.b = torch.tensor(0.0,requires_grad=True)
+
+class MultipleLinearRegression:
+    def __init__(self, t_in, t_out):
+        self._in = t_in
+        self._out = t_out
+        self.w = torch.zeros(t_in.shape[1], requires_grad=True)
+        self.b = torch.tensor(0.0, requires_grad=True)
+
+    def __repr__(self) -> str:
+        return f"MultipleLinearRegression(weights={self.w},bias={self.b})"
 
     @property
     def _loss(self):
         loss = 0.0
-        for i in range(self.t_in.shape[0]):
-            loss += torch.square((torch.dot(self.t_in[i],self.w) + self.b) - self.t_out[i])
-        return (loss)/(2*self.t_in.shape[0])
+        for i in range(self._in.shape[0]):
+            loss += torch.square(
+                (torch.dot(self._in[i], self.w) + self.b) - self._out[i]
+            )
+        return loss / (2 * self._in.shape[0])
 
-
-    def train(self,epochs,l_rate):
-        for i in tqdm(range(epochs),delay=0.25,total=epochs,desc="Training Process"):
+    def train(self, epochs, l_rate):
+        for _ in tqdm(range(epochs), delay=0.25, total=epochs, desc="Training Process"):
             self._loss.backward()
             with torch.no_grad():
                 self.w -= l_rate * self.w.grad
@@ -25,8 +30,16 @@ class Regression:
                 self.w.grad.zero_()
                 self.b.grad.zero_()
 
-    def predict(self,test_data):
-        preds = torch.zeros(test_data.shape[0])
-        for i in range(test_data.shape[0]):
-            preds[i] = torch.dot(test_data[i],self.w) + self.b
-        return preds
+    def predict(self, x, y):
+        for i in range(x.shape[0]):
+            pred = torch.dot(x[i], self.w) + self.b
+            print(
+                f"Predicted {pred:0.2f}    Expected {y[i]}  Diff {torch.abs(pred - y[i])}"
+            )
+
+    @staticmethod
+    def normalize(x):
+        std = torch.min(x, dim=0).values
+        mean = torch.mean(x, dim=0)
+        x_norm = (x - mean) / std
+        return x_norm, mean, std
