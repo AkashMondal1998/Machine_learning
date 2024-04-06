79c3ff020c33b37f701cc3652429e412e9f423cd
Akash Mondal <mondalakash63@gmail.com>
2024-04-06 13:53:05 +0530

    Repr function of Base class and BCELoss added

--- neural_engine/nn.py
+++ neural_engine/nn.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-
-# Todo repr of the base class
-
-
 # Base class
 class Base:
+    def __repr__(self):
+        layers = (f"({layer_name}): {layer}" for layer_name,layer in vars(self).items())
+        return self.__class__.__name__ + '(\n' + '\n'.join(layers) + '\n)'
+    
     def __call__(self, x):
         return self.forward(x)
-
-    def __repr__(self):
-        return str(vars(self))
 
     def parameters(self):
         for layer in vars(self).values():
@@ -43,15 +40,26 @@ class Layer:
         return f"Layer(in_features = {self._in}, out_features = {len(self._neurons)})"
 
     def __call__(self, x):
-        assert x.ndim == 2, "Input must be a 2D matrix"
+        if x.ndim != 2:
+            raise ValueError("Input must be a 2D matrix") 
         return np.concatenate(tuple(neuron(x) for neuron in self._neurons), axis=1)
 
 
 # Mean Squared error loss function
 class MSELoss:
     def __call__(self, x, y):
-        assert y.shape == x.shape, "mat1 and mat2 be must have same shape"
+        if x.shape != y.shape:
+            raise ValueError("mat1 and mat2 be must have same shape")
         return np.mean(np.square(x - y))
+
+
+# Binary CrossEntropy Loss
+# handle numerical instabality
+class BCELoss:
+    def __call__(self, x, y):
+        if x.shape != y.shape:
+            raise ValueError("mat1 and mat2 be must have same shape")
+        return np.mean(-y * np.log(x) + (1-y) * np.log(1-x))
 
 
 # ReLU Activation
@@ -59,9 +67,15 @@ class ReLU:
     def __call__(self, x):
         return np.where(x > 0, x, 0)
 
+    def __repr__(self):
+        return f"ReLU()"
+
 
 # Sigmoid Activation
 class Sigmoid:
     def __call__(self, x):
         max_x = np.max(x)
         return np.exp(x - max_x) / (1 + np.exp(x - max_x))
+
+    def __repr__(self):
+        return f"Sigmoid()"
