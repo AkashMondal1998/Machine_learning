9dc25a7722a1d612c6b736ad098029342ca5ed90
Akash Mondal <mondalakash63@gmail.com>
2024-04-08 23:13:15 +0530

    Simplified linearization

--- neural_engine/nn/nn.py
+++ neural_engine/nn/nn.py
@@ -13,49 +13,29 @@ class Base:
     def parameters(self):
         for layer in vars(self).values():
             if isinstance(layer, Layer):
-                for neuron in layer._neurons:
-                    yield neuron._w
-                    yield neuron._b
+                yield layer._w
+                if layer._b is not None:
+                    yield layer._b
 
 
-# A Neuron
-class Neuron:
-    def __init__(self, in_features, requires_bias):
-        self._w = np.random.rand(in_features, 1)
-        if requires_bias:
-            self._b = np.random.rand(1, 1)
-
-    def __repr__(self):
-        if self._b:
-            return f"Neuron(w={self._w.shape},b={self._b.shape})"
-        return f"Neuron(w={self._w.shape}"
-
-    def __call__(self, x):
-        if not hasattr(self, "_b"):
-            return x @ self._w
-        return x @ self._w + self._b
-
-
-# A Layer of Neurons
+# Layer for linear transformation
 class Layer:
-    def __init__(self, in_features, no_of_neurons, requires_bias=True):
-        self._in = in_features
-        self._requires_bias = requires_bias
-        self._neurons = tuple(Neuron(in_features, requires_bias) for _ in range(no_of_neurons))
+    def __init__(self, in_features, no_of_neurons, bias=True):
+        self._w = np.random.rand(in_features, no_of_neurons)
+        self._b = np.random.rand(1, no_of_neurons) if bias else None
 
     def __repr__(self):
-        return f"Layer(in_features = {self._in}, out_features = {len(self._neurons)}, requires_bias={self._requires_bias})"
+        return f"Layer(in_features = {self._w.shape[0]}, out_features = {(self._w.shape[1])}, requires_bias={True if self._b is not None else False})"
 
     @property
     def weight(self):
-        return np.concatenate(tuple(neuron._w for neuron in self._neurons), axis=1)
+        return self._w
 
     @property
     def bias(self):
-        if self._requires_bias:
-            return np.concatenate(tuple(neuron._b for neuron in self._neurons), axis=1)
+        return self._b
 
     def __call__(self, x):
         if x.ndim != 2:
             raise ValueError("Input must be a 2D matrix")
-        return np.concatenate(tuple(neuron(x) for neuron in self._neurons), axis=1)
+        return x @ self._w.T + self._b
