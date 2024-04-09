c81fe4821108267293cf702ba41a3fb73e46c43f
Akash Mondal <mondalakash63@gmail.com>
2024-04-09 14:45:50 +0530

    Update tensor.py

--- neural_engine/tensor.py
+++ neural_engine/tensor.py
@@ -1,52 +1,22 @@
-from typing import Iterable
+import numpy as np
+from numpy.typing import NDArray
 
 
-# Tensor
-# Not sure if it is correct
+# wraps a numpy array
 class Tensor:
-    def __init__(self, data: Iterable):
-        self._data = tuple(data) if isinstance(data, Iterable) else (data,)
-        self.shape = (len(self._data),) if isinstance(data, Iterable) else (1,)
-        self._prev = set()
+    def __init__(self, data: NDArray, _children=()):
+        assert isinstance(data, (np.ndarray))
+        self.data = data
+        self._prev = set(_children)
+        self.dtype = data.dtype
+        self.shape = data.shape
 
-    def __repr__(self) -> str:
-        rep = f"Tensor({self._data[0]})" if self.shape[0] == 1 else f"Tensor{self._data}"
-        return rep
-
-    def __iter__(self) -> Iterable:
-        return iter(self._data)
+    def __repr__(self):
+        return f"Tensor({self.data})"
 
     def __getitem__(self, item):
-        return self._data[item]
+        out = Tensor(self.data[item]) if isinstance(self.data[item], np.ndarray) else self.data[item]
+        return out
 
-    def __add__(self, other):
-        assert isinstance(other, Tensor)
-        assert self.shape[0] == other.shape[0]
-        tensor = Tensor((x + y for x, y in zip(self._data, other._data)))
-        setattr(tensor, "_prev", set((self, other)))
-        return tensor
-
-    def __sub__(self, other):
-        assert isinstance(other, Tensor)
-        assert self.shape[0] == other.shape[0]
-        tensor = Tensor((x - y for x, y in zip(self._data, other._data)))
-        setattr(tensor, "_prev", set((self, other)))
-        return tensor
-
-    def __mul__(self, other):
-        assert isinstance(other, Tensor)
-        assert self.shape[0] == other.shape[0]
-        tensor = Tensor((x * y for x, y in zip(self._data, other._data)))
-        setattr(tensor, "_prev", set((self, other)))
-        return tensor
-
-    def dot(self, other):
-        assert isinstance(other, Tensor)
-        assert self.shape[0] == other.shape[0]
-        tensor = Tensor(sum(x * y for x, y in zip(self._data, other._data)))
-        setattr(tensor, "_prev", set((self, other)))
-        return tensor
-
-    def value(self):
-        assert self.shape[0] == 1, "Can only done with a Tensor of shape (1,)"
-        return self._data[0]
+    def __iter__(self):
+        return self.data
