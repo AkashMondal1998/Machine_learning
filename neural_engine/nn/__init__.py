7cdb398df4cba864c89d541da4e1480eb771ae6b
Akash Mondal <mondalakash63@gmail.com>
2024-04-09 17:22:41 +0530

    Tensor now supports new ops

--- neural_engine/nn/__init__.py
+++ neural_engine/nn/__init__.py
@@ -1,3 +1,2 @@
 from .nn import Base, Layer
-from .activations import ReLU, Sigmoid
 from .losses import MSELoss, BCELoss, CrossEntropyLoss
--- neural_engine/tensor.py
+++ neural_engine/tensor.py
@@ -1,22 +1,56 @@
 import numpy as np
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike
 
 
-# wraps a numpy array
 class Tensor:
-    def __init__(self, data: NDArray, _children=()):
-        assert isinstance(data, (np.ndarray))
-        self.data = data
+    def __init__(self, data: ArrayLike, dtype=np.float32, _children=()):
+        self._data = data if isinstance(data, np.ndarray) else np.array(data, dtype=dtype)
         self._prev = set(_children)
-        self.dtype = data.dtype
-        self.shape = data.shape
+        self.dtype = self._data.dtype
+        self.shape = self._data.shape
+        self.ndim = self._data.ndim
 
     def __repr__(self):
-        return f"Tensor({self.data})"
+        return f"Tensor({self._data}, dtype={self.dtype})"
 
     def __getitem__(self, item):
-        out = Tensor(self.data[item]) if isinstance(self.data[item], np.ndarray) else self.data[item]
+        out = Tensor(self._data[item]) if isinstance(self._data[item], np.ndarray) else self._data[item]
         return out
 
     def __iter__(self):
-        return self.data
+        return self._data
+
+    def __add__(self, other):
+        assert isinstance(other, Tensor)
+        out = self._data + other._data
+        return Tensor(out, out.dtype, (self, other))
+
+    def __mul__(self, other):
+        assert isinstance(other, Tensor)
+        out = self._data * other._data
+        return Tensor(out, out.dtype, (self, other))
+
+    def dot(self, other):
+        assert isinstance(other, Tensor)
+        return np.dot(self._data, other._data)
+
+    def matmul(self, other):
+        out = np.matmul(self._data, other._data)
+        return Tensor(out, out.dtype, (self, other))
+
+    def log(self):
+        out = np.log(self._data)
+        return Tensor(out, out.dtype, (self,))
+
+    def relu(self):
+        out = self._data * (self._data > 0)
+        return Tensor(out, out.dtype, (self,))
+
+    def max(self):
+        out = np.max(self._data)
+        return Tensor(out, out.dtype)
+
+    def sigmoid(self):
+        max_x = np.max(self._data)
+        out = np.exp(self._data - max_x) / (1 + np.exp(self._data - max_x))
+        return Tensor(out, out.dtype, (self,))
