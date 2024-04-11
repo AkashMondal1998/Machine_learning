cdf67a936113196f59b4e4f2755f37c1f56e6902
Akash Mondal <mondalakash63@gmail.com>
2024-04-11 19:04:28 +0530

    Working on auto grad

--- neural_engine/tensor.py
+++ neural_engine/tensor.py
@@ -13,12 +13,11 @@ class Tensor:
         self.dtype = self._data.dtype
         self.shape = self._data.shape
         self.ndim = self._data.ndim
+        self._backward = lambda: None
+        self.grad = np.zeros_like(self._data)
 
     def __repr__(self):
-        return f"Tensor({self._data}, dtype={self.dtype})"
-
-    """def __getitem__(self, item):
-        return self._data.__getitem__(item)"""
+        return f"Tensor({self._data},grad={self.grad})"
 
     def __iter__(self):
         return self._data
@@ -32,7 +31,14 @@ class Tensor:
     def __add__(self, other):
         other = Tensor(other) if not isinstance(other, Tensor) else other
         out = self._data + other._data
-        return Tensor(out, out.dtype, (self, other))
+        out = Tensor(out, out.dtype, (self, other))
+
+        def _backward():
+            self.grad += np.eye(*self.shape) * out.grad
+            other.grad += np.eye(*other.shape) * out.grad
+
+        out._backward = _backward
+        return out
 
     def __sub__(self, other):
         other = Tensor(other) if not isinstance(other, Tensor) else other
@@ -43,7 +49,6 @@ class Tensor:
         return self - other
 
     def __mul__(self, other):
-        assert isinstance(other, Tensor)
         other = Tensor(other) if not isinstance(other, Tensor) else other
         out = self._data * other._data
         return Tensor(out, out.dtype, (self, other))
@@ -57,17 +62,28 @@ class Tensor:
         return Tensor(out, out.data, (self,))
 
     def __truediv__(self, other):
-        assert isinstance(other, Tensor)
+        other = Tensor(other) if not isinstance(other, Tensor) else other
         out = self._data / other._data
         return Tensor(out, out.dtype, (self, other))
 
     def dot(self, other):
         assert isinstance(other, Tensor)
+        if self.ndim > 1 and other.ndim > 1:
+            out = np.dot(self._data, other._data)
+            out = Tensor(out, out.dtype, (self, other))
+
+            def _backward():
+                self.grad += other._data.T * out.grad
+                other.grad += self._data.T * out.grad
+
+            out._backward = _backward
+
+            return out
+
         return np.dot(self._data, other._data)
 
     def matmul(self, other):
-        out = np.matmul(self._data, other._data)
-        return Tensor(out, out.dtype, (self, other))
+        return self.dot(other)
 
     def log(self):
         out = np.log(self._data)
@@ -100,3 +116,24 @@ class Tensor:
     def sum(self, axis=None, keepdims=False):
         out = np.sum(self._data, axis=axis, keepdims=keepdims)
         return Tensor(out, out.dtype, (self,))
+
+    def flatten(self):
+        out = self._data.flatten()
+        return Tensor(out, out.dtype, (self,))
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        assert self._prev != set(), "cannot call backward on a leaf node"
+        build_topo(self)
+        self.grad = np.array(1.0)
+        for node in reversed(topo):
+            node._backward()
