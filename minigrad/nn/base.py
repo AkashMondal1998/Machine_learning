54df6032002087ad04125301a7afb97b7cb85389
Akash Mondal <mondalakash63@gmail.com>
2024-04-19 11:15:48 +0530

    Fixed annotations

--- minigrad/nn/base.py
+++ minigrad/nn/base.py
@@ -14,17 +14,16 @@ class Base:
     modules = (f"({module_name}): {module}" for module_name, module in vars(self).items())
     return self.__class__.__name__ + "(\n    " + "\n    ".join(modules) + "\n)"
 
-  def __call__(self, x: "Tensor"):
+  def __call__(self, x: Tensor):
     return self.forward(x)
 
-  def parameters(self) -> Generator["Tensor", None, None]:
-    for layer in vars(self).values():
-      if isinstance(layer, Linear):
-        yield layer._w
-        if layer._b is not None:
-          yield layer._b
+  def parameters(self) -> Generator[Tensor, None, None]:
+    for module in vars(self).values():
+      if isinstance(module, Linear):
+        yield module._w
+        if module._b is not None:
+          yield module._b
 
-  # do not know if this new implementation works or not for all scenarios
   def zero_grad(self):
     for w, b in pairwise(self.parameters()):
       w.grad = np.zeros_like(w.grad)
