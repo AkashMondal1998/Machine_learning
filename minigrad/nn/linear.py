698465bc3d47f6c1379e49d299d95678b8b1577f
Akash Mondal <mondalakash63@gmail.com>
2024-04-22 04:34:49 +0530

    cleaned up

--- minigrad/nn/linear.py
+++ minigrad/nn/linear.py
@@ -5,25 +5,16 @@ from minigrad import Tensor
 
 # linear transformation
 class Linear:
-  def __init__(self, in_features: int, out_features: int, bias=True) -> None:
+  def __init__(self, in_features: int, out_features: int) -> None:
     self._w = Tensor.normal(0.0, np.sqrt(2 / (in_features + out_features)), (in_features, out_features))
-    self._b = Tensor.zeros(1, out_features) if bias else None
 
   def __repr__(self):
-    return f"Linear(in_features = {self._w.shape[0]}, out_features = {(self._w.shape[1])}, requires_bias={True if self._b is not None else False})"
+    return f"Linear(in_features = {self._w.shape[0]}, out_features = {(self._w.shape[1])})"
 
   @property
   def weight(self):
     return self._w
 
-  @property
-  def bias(self):
-    return self._b
-
   def __call__(self, x: Tensor):
-    if x.ndim != 2:
-      raise ValueError("Input must be a 2D matrix")
-    if self._b is None:
-      return x @ self._w
-    else:
-      return x @ self._w + self._b
+    assert x.ndim == 2
+    return x.dot(self._w)
