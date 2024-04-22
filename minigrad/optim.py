cd471b8f5128df3c42002cbf8f3ac078e86e9837
Akash Mondal <mondalakash63@gmail.com>
2024-04-22 14:15:36 +0530

    RMSProp and Adam done

--- minigrad/optim.py
+++ minigrad/optim.py
@@ -21,17 +21,36 @@ class SGD(Optimizer):
       param.data -= self.lr * param.grad
 
 
-# Todo
-class Adam(Optimizer):
-  def __init__(self, parameters, b1=0.9, b2=0.999, e=1e-8, lr=1e-3):
+class RMSProp(Optimizer):
+  def __init__(self, parameters, epsilon=1e-8, beta=0.9, lr=0.001):
     super().__init__(parameters)
-    self.beta1 = b1
-    self.beta2 = b2
-    self.ep = e
-    self.m = np.array(0, dtype=np.float32)
-    self.v = np.array(0, dtype=np.float32)
+    self.ep = epsilon
+    self.b = beta
+    self.m = [np.zeros_like(t.data) for t in self.params]
+    self.lr = lr
+
+  def step(self):
+    for i, param in enumerate(self.params):
+      self.m[i] = self.b * self.m[i] + (1 - self.b) * param.grad**2
+      param.data -= self.lr / (np.sqrt(self.m[i]) + self.ep) * param.grad
+
+
+class Adam(Optimizer):
+  def __init__(self, parameters, beta1=0.9, beta2=0.999, epsilon=1e-8, lr=0.001):
+    super().__init__(parameters)
+    self.b1 = beta1
+    self.b2 = beta2
+    self.ep = epsilon
+    self.lr = lr
+    self.m = [np.zeros_like(t.data) for t in self.params]
+    self.v = [np.zeros_like(t.data) for t in self.params]
     self.t = 0
 
   def step(self):
-    for param in self._params:
-      pass
+    self.t += 1
+    for i, param in enumerate(self.params):
+      self.m[i] = self.b1 * self.m[i] + (1 - self.b1) * param.grad
+      self.v[i] = self.b2 * self.v[i] + (1 - self.b2) * param.grad**2
+      m_t = self.m[i] / (1 - self.b1**self.t)
+      v_t = self.v[i] / (1 - self.b2**self.t)
+      param.data -= self.lr * (m_t / np.sqrt(v_t) + self.ep)
