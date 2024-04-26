0729c637b3b14f047bc0ad9140c4d5c5e52dd3ec
Akash Mondal <mondalakash63@gmail.com>
2024-04-26 11:58:35 +0530

    Mnsit now fetched from remote

--- minigrad/nn/datasets.py
+++ minigrad/nn/datasets.py
@@ -1,12 +1,5 @@
 import numpy as np
-from minigrad.utils import _fetch_fashion, _load_mnsit
-
-def get_mnist():
-    X_train = _load_mnsit("train-images-idx3-ubyte.gz",16).reshape(-1,28*28).astype(np.float32)
-    Y_train = _load_mnsit("train-labels-idx1-ubyte.gz",8).reshape(-1,).astype(np.int32)
-    X_test = _load_mnsit("t10k-images-idx3-ubyte.gz",16).reshape(-1,28*28).astype(np.float32)
-    Y_test = _load_mnsit("t10k-labels-idx1-ubyte.gz",8).reshape(-1,).astype(np.int32)
-    return X_train, Y_train, X_test, Y_test
+from minigrad.utils import _fetch_fashion, _load_mnsit,_fetch_mnist
 
 def get_fashion_mnist():
     X_train = _fetch_fashion("train-images-idx3-ubyte.gz",16).reshape(-1,28*28).astype(np.float32)
@@ -14,3 +7,10 @@ def get_fashion_mnist():
     X_test = _fetch_fashion("t10k-images-idx3-ubyte.gz",16).reshape(-1,28*28).astype(np.float32)
     Y_test = _fetch_fashion("t10k-labels-idx1-ubyte.gz",8).reshape(-1,).astype(np.int32)
     return X_train,Y_train,X_test,Y_test
+
+def get_mnist():
+    X_train = _fetch_mnist("train-images-idx3-ubyte.gz",16).reshape(-1,28*28).astype(np.float32)
+    Y_train = _fetch_mnist("train-labels-idx1-ubyte.gz",8).reshape(-1,).astype(np.int32)
+    X_test = _fetch_mnist("t10k-images-idx3-ubyte.gz",16).reshape(-1,28*28).astype(np.float32)
+    Y_test = _fetch_mnist("t10k-labels-idx1-ubyte.gz",8).reshape(-1,).astype(np.int32)
+    return X_train, Y_train, X_test, Y_test
--- minigrad/utils.py
+++ minigrad/utils.py
@@ -2,15 +2,18 @@ import gzip,os
 import numpy as np
 from urllib.request import urlretrieve
 
-def _load_mnsit(file,offset):
-    with gzip.open(os.path.join(os.getcwd(),"datasets/mnist",file)) as f:
-        array = np.frombuffer(f.read()[offset:],dtype=np.uint8)
-    return array
-
 def _fetch_fashion(file,offset):
     p = os.path.join(os.getcwd(),"datasets/fashion_mnist/")
     os.makedirs(p,exist_ok=True)
-    if not os.path.exists(p+file): urlretrieve("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"+file,p+file)
+    if not os.path.isfile(p+file): urlretrieve("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"+file,p+file)
+    with gzip.open((p+file)) as f:
+        array = np.frombuffer(f.read()[offset:],dtype=np.uint8)
+    return array
+
+def _fetch_mnist(file,offset):
+    p = os.path.join(os.getcwd(),"datasets/mnist/")
+    os.makedirs(p,exist_ok=True)
+    if not os.path.isfile(p+file): urlretrieve("https://storage.googleapis.com/cvdf-datasets/mnist/"+file,p+file)
     with gzip.open((p+file)) as f:
         array = np.frombuffer(f.read()[offset:],dtype=np.uint8)
     return array
