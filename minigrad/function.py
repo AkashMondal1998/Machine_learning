Changes - Backprop for ops involving scalar tensor and tensor  with higher dims fixed - Backward for Sum op cleaned up - Helper functions cleaned up

minigrad/function.py
@@ -93,8 +93,7 @@ class Sum(Function):
     return x.sum(axis=axis, keepdims=keepdims)
 
   def backward(self, grad_out):
-    ret = expand(grad_out, self.axis, self.x.shape) if self.axis else np.ones_like(self.x) * grad_out
-    return ret
+    return expand(grad_out, self.axis, self.x.shape)
 
 
 class Neg(Function):

minigrad/helpers.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 
-def expand(grad_out, axis, in_shape):
-  if grad_out.shape == tuple(): grad_out = grad_out.reshape(1,)
-  if len(grad_out.shape) < len(in_shape): grad_out = grad_out[None, :] if axis == 0 else grad_out[:, None]
-  return np.broadcast_to(grad_out, in_shape)
+def expand(x, axis, in_shape):
+  if len(x.shape) < len(in_shape):
+    if x.shape == tuple(): x = x.reshape(1,)
+    if axis is not None: x = np.expand_dims(x, axis=axis)
+  return np.broadcast_to(x, in_shape)
 
 
-def reduce(grad_out, in_shape):
+def reduce(x, in_shape):
   keepdims = True if len(in_shape) > 1 else False
-  if len(in_shape) < len(grad_out.shape): in_shape = (1,) + in_shape
-  reduce_axis = tuple(i for i in range(len(grad_out.shape)) if in_shape[i] == 1 and grad_out.shape[i] > 1)
-  return grad_out.sum(axis=reduce_axis, keepdims=keepdims)
+  if len(in_shape) < len(x.shape): in_shape = (1,) * (len(x.shape) - len(in_shape)) + in_shape
+  reduce_axis = tuple(i for i in range(len(x.shape)) if in_shape[i] == 1 and x.shape[i] > 1)
+  return x.sum(axis=reduce_axis, keepdims=keepdims)